bbcfba515d85530297d052493cbd5ea3c17a3a9d
mvabrothers <mvabrothers@yandex.ru>
2019-01-22 17:00:22 +0300

    message

--- Day1/app.ts
+++ Day1/app.ts
@@ -3,6 +3,9 @@ import { read } from "./console";
 async function main(): Promise<void>
 {
     //Здесь необходимо писать реализацию
+    console.log('Как Вас зовут?')
+    const str = await read()
+    console.log('Hello '+str)
 }
 
 main();
