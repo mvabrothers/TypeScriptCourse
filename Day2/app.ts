eb4aec2a11f607e7494bfac56a12900a29d888e0
mvabrothers <mvabrothers@gmail.com>
2019-01-29 12:38:24 +0300

    Day2_v1

--- Day2/app.ts
+++ Day2/app.ts
@@ -1,8 +1,18 @@
 import { read } from "./console";
+import { Z_ASCII } from "zlib";
 
 async function main(): Promise<void>
 {
     //Здесь необходимо писать реализацию
+
+    console.log('Введите одну латинскую букву');
+    const str = await read();
+    if (a>str>z) {
+        console.log('Правильно!');
+        else {
+           console.log('Неправильно');
+        }
+    }
 }
 
 main();
